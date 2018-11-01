b311087e7fc96d83f2d84e20601ccb492a5288b9
Gentry <gma23@txstate.edu>
2018-11-01 15:13:43 -0500

    removed random return

--- Scripts/calculate_walks.py
+++ Scripts/calculate_walks.py
@@ -69,7 +69,6 @@ def findWalk(devices, sTime, sLocation, cTime, cLocation, length, outfile, date)
 def compareList (list1, list2):
     #print ("comparing ", list1, " to ", list2)
     return set(list1).issubset(set(list2))
-    return False
 
 fileFile = open('files.txt')
 outFile = open("walks_over_Austin.json", 'a+')
