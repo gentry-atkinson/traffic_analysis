9ac0f645da069bfea8d8525633806394264aa349
Gentry Atkinson <gma23@txstate.edu>
2018-12-03 15:51:23 -0600

    updated walk counting script

--- Scripts/count_walks.py
+++ Scripts/count_walks.py
@@ -8,19 +8,44 @@ inFileName = "/home/user/Desktop/cs7311_data/walks/"
 
 counter = 0
 
+totalsForDevices = dict()
+totalsForDistance = dict()
+
 while(fileFile):
     try:
         thisFileName = inFileName + fileFile.readline()
-        print(thisFileName)
-        inFile = open("/home/user/Desktop/cs7311_data/walks/walk_ITMF_2015-12-31.json", 'r')
+        thisFileName = thisFileName[:-1]
+        inFile = open(thisFileName, 'r')
         counter += 1
-        wait = input("Press something")
+        #wait = input("Press something")
     except:
         print ("End of File")
         break
 
-    #item = inFile.readline()
-    #print(item)
+    while(inFile):
+        try:
+            line = inFile.readline()
+            item = json.loads(line)
+        except:
+            #print("File ", counter, " has processed.")
+            break
+        try:
+            totalsForDevices[item["Num_Devices"]] += 1
+        except:
+            totalsForDevices[item["Num_Devices"]] = 1
+
+        try:
+            totalsForDistance[item["Walk_Length"]] += 1
+        except:
+            totalsForDistance[item["Walk_Length"]] = 1
+    #end of while(inFile)
+
 #end of while(fileFile)
+
+for value in sorted(totalsForDevices.keys()):
+    print ("Number of walks with ", value, " devices: ", totalsForDevices[value])
+
+for value in sorted(totalsForDistance.keys()):
+    print ("Number of walks with ", value, " distance covered: ", totalsForDistance[value])
 
 print(counter, " total files read.")
