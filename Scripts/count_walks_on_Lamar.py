6caf6a03dd8089edb27854229f7e1f5d8606dc96
Gentry Atkinson <gma23@txstate.edu>
2018-12-04 12:09:18 -0600

    counted walks on Lamar

---
+++ Scripts/count_walks_on_Lamar.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import json
+
+fileFile = open("walks_fileNames.txt", 'r')
+
+inFileName = "/home/user/Desktop/cs7311_data/walks/"
+
+counter = 0
+
+totalsForDevices = dict()
+totalsForDistance = dict()
+totalsForDevOverDis = dict()
+
+while(fileFile):
+    try:
+        thisFileName = inFileName + fileFile.readline()
+        thisFileName = thisFileName[:-1]
+        inFile = open(thisFileName, 'r')
+        counter += 1
+        #wait = input("Press something")
+    except:
+        print ("End of File")
+        break
+
+    while(inFile):
+        try:
+            line = inFile.readline()
+            item = json.loads(line)
+        except:
+            #print("File ", counter, " has processed.")
+            break
+        origin = item["Origin"]
+        destination = item["Destination"]
+
+        if ("lamar" in origin and "lamar" in destination):
+            try:
+                totalsForDevices[item["Num_Devices"]] += 1
+            except KeyError:
+                totalsForDevices[item["Num_Devices"]] = 1
+
+            try:
+                totalsForDistance[item["Walk_Length"]] += 1
+            except KeyError:
+                totalsForDistance[item["Walk_Length"]] = 1
+
+            try:
+                totalsForDevOverDis[item["Num_Devices"]][item["Walk_Length"]] += 1
+            except KeyError:
+                try:
+                    totalsForDevOverDis[item["Num_Devices"]][item["Walk_Length"]] = 1
+                except KeyError:
+                    #print("Creating dict for totalsForDevOverDis[item[", item["Num_Devices"], "]]")
+                    totalsForDevOverDis[item["Num_Devices"]] = dict()
+                    totalsForDevOverDis[item["Num_Devices"]][item["Walk_Length"]] = 1
+    #end of while(inFile)
+
+#end of while(fileFile)
+
+print("****************Walks on Lamar**************\n\n")
+
+print ("*************Number of Devices*************")
+
+for value in (totalsForDevices.keys()):
+    print ("Number of walks with ", value, " devices: ", totalsForDevices[value])
+
+print ("*************Total Distance****************")
+
+for value in (totalsForDistance.keys()):
+    print ("Number of walks with ", value, " distance covered: ", totalsForDistance[value])
+
+print ("********Devices Over Distance**************")
+
+for numDev in totalsForDevOverDis.keys():
+    for walkLength in totalsForDevOverDis[numDev].keys():
+        print ("Devices: ", numDev, "\tLength: ", walkLength, "\tTotal Walks: ", totalsForDevOverDis[numDev][walkLength])
+
+print(counter, " total files read.")
